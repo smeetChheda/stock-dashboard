dee6c79e921ff22498e5338250560d7aeea0ac09
smeetChheda <contact.smeet@gmail.com>
2023-08-10 18:01:33 -0400

    updated header with dark mode icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Search from "./Search";
+import ThemeIcon from "./ThemeIcon";
+
 export default function Header({ name }) {
     return (
-        <div className="xl:px-32">
-            <h1 className="text-5xl">{name}</h1>
-            <Search />
-        </div>
+        <>
+            <div className="xl:px-32">
+                <h1 className="text-5xl">{name}</h1>
+                <Search />
+            </div>
+            <ThemeIcon />
+        </>
+        
     )
 }
